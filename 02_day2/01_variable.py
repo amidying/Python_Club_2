86a9491c8ba47b1a1e1a54aa65a82a5b544ebdf1
Ali Neaz <neazali77@gmail.com>
2023-03-09 01:48:45 -0500

    add type casting

---
+++ 02_day2/01_variable.py
@@ -0,0 +1,19 @@
+
+
+name = "Asif"
+age = 19
+location = None
+married = False
+height = 6.1
+
+print("Your name is: ",name)
+print(19)
+print(location)
+print(married)
+print(height)
+
+print(type(age))
+print(type(location))
+print(type(height))
+print(type(married))
+print(type(name))
---
+++ 03_day3/01_input_function.py
@@ -0,0 +1,16 @@
+
+age = 23
+print(age)
+
+# age = input("Enter your age: ")
+# print(age)
+# input takes values as string
+
+# typecasting : suppose you have integer type data and you want it to be string
+age = 12
+print(type(age))
+age = str(age)
+print(type(age))
+age3 = float(age)
+print(type(age3))
+# ---------- input function always takes value as string
